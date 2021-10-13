7deeb4a9db996174d5daf811b7d6747c09f06e1b
Daniel <dancoches@gmail.com>
2021-10-12 19:58:26 -0500

    Finished Item Designer

--- Assets/Resources/ItemData/Scripts/ItemData.cs
+++ Assets/Resources/ItemData/Scripts/ItemData.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class ItemData : ScriptableObject
 {
     public GameObject prefab;
-    public float price;
-    public float maxHealth;
-    public float maxEnergy;
-    public float critChance;
-    public float power;
-    public string _name;
+    public Sprite visualSprite;
+    public float price; //how expensive it is
+    public bool canBreak;
+    public float maxHealth; //how much health it has if it can break
+    public float maxCharge; //cooldown charge
+    public float critChance; //chance for critical hit or heal
+    public float power; //this is damage, health recovered, or a number needed to calculate a change in game
+    public string _name; //name of item
 }
